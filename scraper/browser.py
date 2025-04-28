247fbc2b4589b5f0929c50e457747a4dbac28d1d
Jose Torres <jtorresocc@gmail.com>
2025-04-28 15:10:44 -0700

    Added boilerplate browser.py

---
+++ scraper/browser.py
@@ -0,0 +1,17 @@
+# scraper/browser.py
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+def get_driver(headless=True):
+    options = Options()
+    if headless:
+        options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+    return driver
